1e524ff3c50f0e725fa0d83af15242f20939eb41
hanqinilnix <77395961+hanqinilnix@users.noreply.github.com>
2022-02-21 22:48:52 +0800

    Update Remark.java
    Include javadocs for constructor

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for Remark class
+     * @param remark of a Person
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
